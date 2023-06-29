19fe010c50d2b2a5e668be72fe4e40c188d9ac0b
VladimirL <vleushkin@gmail.com>
2023-06-29 08:42:20 +0300

    node template added

--- src/lidar_odometry_node.cpp
+++ src/lidar_odometry_node.cpp
@@ -1,10 +1,30 @@
-#include <cstdio>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+
+class LidarOdometryNode : public rclcpp::Node
+{
+public:
+    LidarOdometryNode()
+            : Node("lidar_odometry_node")
+    {
+        cloud_subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>("/lidar_points", rclcpp::QoS(10),
+                                                                                       std::bind(&LidarOdometryNode::onPointCloudCallback, this, std::placeholders::_1));
+    }
+
+protected:
+    void onPointCloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg_ptr)
+    {
+
+    }
+
+private:
+    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr cloud_subscription_;
+};
 
 int main(int argc, char ** argv)
 {
-  (void) argc;
-  (void) argv;
-
-  printf("hello world lidar_odometry package\n");
-  return 0;
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<LidarOdometryNode>());
+    rclcpp::shutdown();
+    return 0;
 }
